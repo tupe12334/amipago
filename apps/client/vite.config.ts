6321cb9b0d693af3c63cdd4316785fe7c60a7167
tupe12334 <ofek.gabay.he@gmail.com>
2025-03-09 20:52:35 +0200

    Remove reactRouter plugin from Vite configuration and add new words to cspell dictionary

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -9,7 +9,7 @@ const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react(), tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
