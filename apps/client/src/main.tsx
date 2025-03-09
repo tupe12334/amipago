2b064c372f9b817bc14a99913ce8eddc0fdeb8c1
tupe12334 <ofek.gabay.he@gmail.com>
2025-03-09 21:09:27 +0200

    Update HTML doctype, add Google Fonts, and set body font-family in CSS

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
