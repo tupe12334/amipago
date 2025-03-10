64adf364bd4f78a14941492bfe40db54996d0c83
tupe12334 <ofek.gabay.he@gmail.com>
2025-03-11 00:32:00 +0200

    Refactor BackButton component for improved accessibility and styling

--- apps/client/src/components/BackButton/BackButton.tsx
+++ apps/client/src/components/BackButton/BackButton.tsx
@@ -3,10 +3,10 @@ import { FaArrowRight } from "react-icons/fa6";
 export function BackButton() {
   return (
     <button
-      className="h-full flex items-center justify-center shadow-lg hover:bg-blue-600 active:scale-95 focus:outline-none focus:ring-2 focus:ring-blue-400 transition-all duration-300 transform hover:scale-105"
+      className="h-full flex items-center justify-center active:scale-95 focus:outline-none focus:ring-2 transition-all duration-300 transform hover:scale-105"
       aria-label="Back"
     >
-      <FaArrowRight />
+      <FaArrowRight className="text-4xl" />
     </button>
   );
 }
