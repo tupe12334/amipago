0809f8958fd0a81cfb2696cb5489e2892d645403
tupe12334 <ofek.gabay.he@gmail.com>
2025-03-11 00:05:19 +0200

    Add select-none class to main container for improved text selection handling

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <div className="m-0">
+  <div className="m-0 select-none">
     <React.StrictMode>
       <BrowserRouter>
         <Routes>
