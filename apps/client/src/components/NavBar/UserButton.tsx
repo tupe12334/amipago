73b11f581be795fe74b013ccd20fe2cc9159c914
tupe12334 <ofek.gabay.he@gmail.com>
2025-03-12 02:39:01 +0200

    Remove react-icons dependency and replace FaUser icon with Font Awesome in UserButton component

--- apps/client/src/components/NavBar/UserButton.tsx
+++ apps/client/src/components/NavBar/UserButton.tsx
@@ -1,5 +1,4 @@
 import { FC } from "react";
-import { FaUser } from "react-icons/fa6";
 
 export const UserButton: FC = () => {
   return (
@@ -7,7 +6,7 @@ export const UserButton: FC = () => {
       className="flex items-center justify-center w-14 h-14 rounded-full bg-blue-500 text-white shadow-lg hover:bg-blue-600 active:scale-95 focus:outline-none focus:ring-2 focus:ring-blue-400 transition-all duration-300 transform hover:scale-105"
       aria-label="User Profile"
     >
-      <FaUser />
+      <i className="fa fa-user" aria-hidden="true"></i>
     </button>
   );
 };
