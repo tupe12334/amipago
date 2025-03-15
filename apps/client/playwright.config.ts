9584ef9cdfe80041448f73e04d0615ab1aa91b99
tupe12334 <ofek.gabay.he@gmail.com>
2025-03-15 17:09:55 +0200

    Update Playwright configuration to change reporter to 'null' and increase worker count in CI

--- apps/client/playwright.config.ts
+++ apps/client/playwright.config.ts
@@ -21,8 +21,8 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: "html",
+  reporter: "null",
+  workers: process.env.CI ? 1 : 8,
   use: {
     baseURL: baseUrl,
     trace: "on",
