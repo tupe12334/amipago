1fead701c71bba52e99fef50cd1b074ca850be9b
tupe12334 <ofek.gabay.he@gmail.com>
2025-03-15 23:00:02 +0200

    Add ESLint and Prettier configuration with unused imports plugin

---
+++ apps/client/.eslintrc.cjs
@@ -0,0 +1,16 @@
+export default {
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  plugins: ["unused-imports"],
+  rules: {
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+  },
+};
