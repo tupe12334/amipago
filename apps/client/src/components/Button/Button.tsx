ec982ebe7f3ef36bc0cf839b0ebc999ae5804817
tupe12334 <ofek.gabay.he@gmail.com>
2025-03-17 21:13:33 +0200

    Remove unused Button component and its associated type definitions

--- apps/client/src/components/Button/Button.tsx
+++
@@ -1,7 +0,0 @@
-export type Props = React.DetailedHTMLProps<
-  React.ButtonHTMLAttributes<HTMLButtonElement>,
-  HTMLButtonElement
->;
-export function Button(props: Props) {
-  <button {...props}></button>;
-}
