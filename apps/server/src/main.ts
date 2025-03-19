f381bc37a4769871a687464875bbcce013e7a12d
tupe12334 <ofek.gabay.he@gmail.com>
2025-03-20 00:37:07 +0200

    feat(swagger): save Swagger JSON documentation to swagger.json and update setup

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,13 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule } from '@nestjs/swagger';
+import { writeFile } from 'fs/promises';
+import { join } from 'path';
 import { AppModule } from './app.module';
 import { swaggerDoc } from './swagger/document';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const documentFactory = () => SwaggerModule.createDocument(app, swaggerDoc);
-  SwaggerModule.setup('api', app, documentFactory);
+  const document = SwaggerModule.createDocument(app, swaggerDoc);
+
+  // Save swagger json next to main.ts
+  await writeFile(
+    join(__dirname, '../swagger.json'),
+    JSON.stringify(document, null, 2),
+  );
+
+  SwaggerModule.setup('api', app, () => document, {
+    jsonDocumentUrl: '/api-json',
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
