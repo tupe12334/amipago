139e501e8d40d518e9094bece2b7cca40cacd462
tupe12334 <ofek.gabay.he@gmail.com>
2025-03-20 02:02:46 +0200

    feat(client): add GraphQL schema and update code generation configuration

--- apps/client/codegen.ts
+++ apps/client/codegen.ts
@@ -3,19 +3,27 @@ import { CodegenConfig } from "@graphql-codegen/cli";
 const config: CodegenConfig = {
   schema: "http://localhost:3000/graphql",
   documents: ["./app/**/*.{ts,tsx}"],
+  ignoreNoDocuments: true,
   generates: {
     "./app/generated/": {
       preset: "client",
       presetConfig: {
         gqlTagName: "gql",
       },
+      config: {
+        documentMode: "documentNode",
+        withComponent: true,
+        withHooks: true,
+      },
       plugins: ["typescript", "typescript-operations"],
     },
-    "./graphql.schema.json": {
-      plugins: ["introspection"],
+    "./schema.graphql": {
+      plugins: ["schema-ast"],
+      config: {
+        includeDirectives: true,
+      },
     },
   },
-  ignoreNoDocuments: true,
 };
 
 export default config;
