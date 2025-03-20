92d952e709a84248752d17d87c7fadc9b98ae0db
tupe12334 <ofek.gabay.he@gmail.com>
2025-03-20 02:09:09 +0200

    feat(client): integrate Apollo Client and set up ApolloProvider in main component

---
+++ apps/client/app/apollo/main.ts
@@ -0,0 +1,11 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+
+export const client = new ApolloClient({
+  uri: "http://localhost:3000/",
+  cache: new InMemoryCache(),
+});
--- apps/client/app/main.tsx
+++ apps/client/app/main.tsx
@@ -6,12 +6,16 @@ import { ThemeProvider } from "./theme/ThemeProvider";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import "./firebase/config";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./apollo/main";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <LocalizationProvider dateAdapter={AdapterDateFns}>
       <ThemeProvider>
-        <Router />
+        <ApolloProvider client={client}>
+          <Router />
+        </ApolloProvider>
       </ThemeProvider>
     </LocalizationProvider>
   </React.StrictMode>,
